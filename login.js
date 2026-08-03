144d1e7498f0d0358440bb1137361e0b034fc7cb
Ana Júlia Leite <leite.anajulia25@gmail.com>
2026-08-03 02:43:58 -0300

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-module.exports = function handler(req, res) {
-  const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const base_url = (process.env.BASE_URL || "").replace(/\/$/, "");
-  const redirect_uri = `${base_url}/callback`;
-  const scope = "user-read-currently-playing user-read-recently-played";
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
-  return res.redirect(authUrl);
-};
